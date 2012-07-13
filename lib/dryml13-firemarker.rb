412120930a0d923215f64cc01b5969f574291743
Bryan Larsen <bryan@larsen.st>
2012-07-13 14:52:40 -0400

    add uid & time to metadata to assist in profiling

--- lib/dryml13-firemarker.rb
+++ lib/dryml13-firemarker.rb
@@ -10,7 +10,7 @@ module Dryml
     def wrap_source_with_metadata(content, kind, name, line, *args)
       return content if name.in?(NO_METADATA_TAGS)
       json = DrymlFireMarker.metadata_to_json(@template_path, kind, name, line, *args)
-      "<% safe_concat(%(<!--[dryml]#{json}-->)) %>" + content + "<% safe_concat(%(<!--[/dryml]-->)) %>"
+      "<% safe_concat(%(<!--[dryml]#{json}-->)) %>" + content + "<% safe_concat(%(<!--[/dryml]#{json}-->)) %>"
     end
 
   end
